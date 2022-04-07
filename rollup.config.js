3f12f595eec0aceaab0d83460239004582bdb21d
Leonty Chudinov <leonty.chudinov@gmail.com>
2022-04-07 21:49:50 +0500

    Include minified version
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,22 @@ import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: './src/publicApi.ts',
-  output: {
-    file: 'lib/tsterm.js',
-    format: 'es',
-    sourcemap: true
-  },
+  output: [
+    {
+      file: 'lib/tsterm.js',
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      file: 'lib/tsterm.min.js',
+      format: 'es',
+      sourcemap: true,
+      plugins: [
+        terser()
+      ]
+    }
+  ],
   plugins: [
     typescript(),
-    //terser(),
   ]
 };
