d41e039bf777b961a8c8639ae4ef44c8911f751d
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2023-02-13 12:52:15 +0100

    Added License header and CHANGELOG
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# TSTerm Emulator Changelog
+
+## `1.0.0`
+- Initial coding
+- Added `iife` rollup option
--- rollup.config.iife.js
+++ rollup.config.iife.js
@@ -8,7 +8,15 @@ export default {
       file: 'lib/iife/tsterm.js',
       format: 'iife',
       name: 'org_omp_tsterm_tn3270',
-      sourcemap: true
+      sourcemap: true,
+      banner: '/*\n' +
+        '\tThis program and the accompanying materials are\n' +
+        '\tmade available under the terms of the Eclipse Public License v2.0 which accompanies\n' +
+        '\tthis distribution, and is available at https://www.eclipse.org/legal/epl-v20.html\n' +
+        '\n\tSPDX-License-Identifier: EPL-2.0\n\n' +
+        '\tCopyright Contributors to the Zowe Project.\n' +
+        '\tCopyright Contributors to the Open Mainframe Project\'s TSTerm Project\n' +
+        '*/\n'
     },
     {
       file: 'lib/iife/tsterm.min.js',
